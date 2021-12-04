d629b20a2bbb3c3dddbb94612c9e1862cb3aae8e
tomerzh <tomerzh@post.bgu.ac.il>
2021-12-04 19:12:11 +0200

    first implementation

--- src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -1,9 +1,47 @@
 package bgu.spl.mics.application.objects;
 
+import java.awt.*;
+
 /**
  * Passive object representing a single CPU.
  * Add all the fields described in the assignment as private fields.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+    private int cores;
+    private Container data;
+    private Cluster cluster;
+
+    /**
+     * public constructor
+     */
+    public CPU(){
+        //type from json file
+        //model from json file
+        cluster = Cluster.getInstance();
+    }
+
+    /**
+     * @inv: cores >= 0
+     * @return number of cores in the CPU.
+     */
+    public int getCores() {
+        return cores;
+    }
+
+    /**
+     *
+     * @return data the CPU is currently processing.
+     */
+    public Container getData(){
+        return data;
+    }
+
+    /**
+     *
+     * @return the instance of the singleton cluster.
+     */
+    public Cluster getCluster(){
+        return cluster;
+    }
 }
--- src/main/java/bgu/spl/mics/application/objects/GPU.java
+++ src/main/java/bgu/spl/mics/application/objects/GPU.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application.objects;
 
+import java.awt.*;
+
 /**
  * Passive object representing a single GPU.
  * Add all the fields described in the assignment as private fields.
@@ -12,5 +14,39 @@ public class GPU {
     enum Type {RTX3090, RTX2080, GTX1080}
 
     private Type type;
+    private Model model;
+    private Cluster cluster;
 
+    /**
+     * public constructor
+     */
+    public GPU(){
+        //type from json file
+        //model from json file
+        cluster = Cluster.getInstance();
+    }
+
+    /**
+     *
+     * @return the type of the GPU.
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     *
+     * @return the model that the GPU is working on.
+     */
+    public Model getModel(){
+        return model;
+    }
+
+    /**
+     *
+     * @return the instance of the singleton cluster.
+     */
+    public Cluster getCluster(){
+        return cluster;
+    }
 }
