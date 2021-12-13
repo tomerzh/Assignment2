afe4800bb528e149a41a56a976f03dc19ebed721
tamhrn <94163279+tamhrn@users.noreply.github.com>
2021-12-13 10:57:33 +0200

    clean

--- src/main/java/bgu/spl/mics/application/objects/ConfrenceInformation.java
+++ src/main/java/bgu/spl/mics/application/objects/ConfrenceInformation.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics.application.objects;
 
+import java.util.Objects;
+
 /**
  * Passive object representing information on a conference.
  * Add fields and methods to this class as you see fit (including public methods and constructors).
@@ -8,4 +10,33 @@ public class ConfrenceInformation {
 
     private String name;
     private int date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConfrenceInformation that = (ConfrenceInformation) o;
+        return date == that.date && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, date);
+    }
 }
