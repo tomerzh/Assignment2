65f3e43e69ce106787809651cfb57684d042bedf
tomerzh <tomerzh@post.bgu.ac.il>
2021-12-13 13:48:54 +0200

    gpuservice

--- src/main/java/bgu/spl/mics/application/messages/TestModelEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/TestModelEvent.java
@@ -1,8 +1,9 @@
 package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Event;
+import bgu.spl.mics.application.objects.Model;
 
-public class TestModelEvent implements Event<Integer> {
+public class TestModelEvent implements Event<Model> {
     String name = "TestModelEvent";
 
     public String getName(){
