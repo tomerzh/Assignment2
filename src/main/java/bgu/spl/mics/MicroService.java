9b02dcda02e9470d69cec7c86e5952fee146c139
tamhrn <94163279+tamhrn@users.noreply.github.com>
2021-12-15 15:04:08 +0200

    1512noon

--- src/main/java/bgu/spl/mics/MicroService.java
+++ src/main/java/bgu/spl/mics/MicroService.java
@@ -26,6 +26,7 @@ public abstract class MicroService implements Runnable {
     private final String name;
     MessageBusImpl bus = MessageBusImpl.getInstance();
     protected HashMap<Class<? extends Message>, Callback<? extends Message>> messageToCallbacks;
+//    private  boolean initialized = false;
 
     /**
      * @param name the micro-service name (used mainly for debugging purposes -
@@ -170,4 +171,11 @@ public abstract class MicroService implements Runnable {
         }
     }
 
+//    public void doneInitialize() {
+//        this.initialized = true;
+//    }
+//
+//    public boolean getInitialize(){
+//        return initialized;
+//    }
 }
