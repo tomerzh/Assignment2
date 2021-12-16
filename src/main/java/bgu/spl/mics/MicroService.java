50d936a78ada40c0631e0e1321fefdc488edbf53
tomerzh <tomerzh@post.bgu.ac.il>
2021-12-16 17:16:00 +0200

    Update MicroService.java

--- src/main/java/bgu/spl/mics/MicroService.java
+++ src/main/java/bgu/spl/mics/MicroService.java
@@ -172,6 +172,7 @@ public abstract class MicroService implements Runnable {
                 }
             }catch (InterruptedException exception){}
         }
+        bus.unregister(this);
     }
 
     public void doneInitialize() {
